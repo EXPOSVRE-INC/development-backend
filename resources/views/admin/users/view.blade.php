681c2690ce76c7cd555dbf5e06f729d22345d1ea
monachouhan <mona@amplework.com>
2024-12-02 10:49:12 +0530

    Refactored user view page layout and design, updated CSS files

--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -1,155 +1,114 @@
 @extends('adminlte::page')
 
 @push('css')
-    <link href="//cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+<link href="//cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
 @endpush
 @section('content')
+<div class="container">
+    <div>
+        <nav aria-label="breadcrumb" class="main-breadcrumb" style="margin-top: 2pc">
+            <ol class="breadcrumb" style="width:74.7%; margin:0px">
+                <h3 style="color: black;margin:auto; font-weight:600;">User Details</h3>
+            </ol>
+        </nav>
+        <div class="row gutters-sm">
+            <div class="col-md-9">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <div class="d-flex justify-content-center mb-5">
+                                <div class="d-flex flex-column align-items-center  text-align-center">
+                                    <img src="{{ $user->getFirstMediaUrl('preview') ?: asset('profileImg.jpeg') }}"
+                                        onerror="this.onerror=null; this.src='{{ asset('profileImg.jpeg') }}';"
+                                        alt="Admin" class="rounded-circle" width="150" />
+                                    <div class="mt-3">
+                                        <h4>{{($user->profile && $user->profile != null ? $user->profile->firstName . '
+                                            ' .$user->profile->lastName : '')}}
+                                            <span class="ml-2" style="color:#52d65d"> {!! $user->verify ? '<i
+                                                    id="verify_status" class="far fa-fw fa-check-circle"
+                                                    aria-hidden="true"></i>' : '<i id="verify_status"
+                                                    class="far fa-fw fa-circle" aria-hidden="true"></i>' !!}</span>
+                                        </h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-<div class="container emp-profile">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="profile-img">
-                    <img src="{{$user->getFirstMediaUrl('preview')}}" alt=""/>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="profile-head">
-                    <h5>
-                        {{($user->profile && $user->profile != null ? $user->profile->firstName . ' ' .$user->profile->lastName : '')}}
-                        {!! $user->verify ? '<i id="verify_status" class="far fa-fw fa-check-circle" aria-hidden="true"></i>' : '<i id="verify_status" class="far fa-fw fa-circle" aria-hidden="true"></i>' !!}
-                    </h5>
-                    <h6>
-                        {{($user->profile && $user->profile != null) ? $user->profile->jobTitle : ''}}
-                    </h6>
-                    <ul class="nav nav-tabs" id="myTab" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
-                        </li>
-{{--                        <li class="nav-item">--}}
-{{--                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>--}}
-{{--                        </li>--}}
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-2">
-                <form id="myForm" name="myForm" action="{{route('users-verify')}}" method="post">
-                    Verify:  <input id="toggle" type="checkbox" data-onstyle="outline-danger" data-toggle="toggle" {{$user->verify ? 'checked' : ''}}>
-                </form>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-
-            </div>
-            <div class="col-md-8">
-                <div class="tab-content profile-tab" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Username</label>
+                        <div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <h6 class="mb-0">UserName :</h6>
+                                </div>
+                                <div class="col-sm-10">
+                                    {{$user->username}}
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <p>{{$user->username}}</p>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <h6 class="mb-0">Full Name : </h6>
+                                </div>
+                                <div class="col-sm-10">
+                                    {{($user->profile && $user->profile != null) ? $user->profile->firstName . ' '
+                                    .$user->profile->lastName : ''}}
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Full Name</label>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <h6 class="mb-0">Email :</h6>
+                                </div>
+                                <div class="col-sm-10">
+                                    {{$user->email}}
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <p>{{($user->profile && $user->profile != null) ? $user->profile->firstName . ' ' .$user->profile->lastName : ''}}</p>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <h6 class="mb-0">Phone :</h6>
+                                </div>
+                                <div class="col-sm-10">
+                                    {{($user->profile && $user->profile != null) ? $user->profile->phone : ''}}
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Email</label>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <h6 class="mb-0">Job :</h6>
+                                </div>
+                                <div class="col-sm-10">
+                                    {{($user->profile && $user->profile != null) ? $user->profile->jobTitle : '--'}}
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <p>{{$user->email}}</p>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <h6 class="mb-0">Job Description :</h6>
+                                </div>
+                                <div class="col-sm-10">
+                                    {{($user->profile && $user->profile != null) ? $user->profile->jobDescription : '--'}}
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Phone</label>
+                            <hr>
+                            <div class="row ml-1">
+                                <form id="myForm" name="myForm" action="{{route('users-verify')}}" method="post">
+                                    <span style="margin-right: 10px">Verify :</span>
+                                    <input id="toggle" class="ms-3" type="checkbox" data-onstyle="outline-danger"
+                                        data-toggle="toggle" {{$user->verify ? 'checked' : ''}} >
                             </div>
-                            <div class="col-md-6">
-                                <p>{{($user->profile && $user->profile != null) ? $user->profile->phone : ''}}</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Job</label>
-                            </div>
-                            <div class="col-md-6">
-                                <p>{{($user->profile && $user->profile != null) ? $user->profile->jobTitle : ''}}</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Job Description</label>
-                            </div>
-                            <div class="col-md-6">
-                                <p>{{($user->profile && $user->profile != null) ? $user->profile->jobDescription : ''}}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Experience</label>
-                            </div>
-                            <div class="col-md-6">
-                                <p>Expert</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Hourly Rate</label>
-                            </div>
-                            <div class="col-md-6">
-                                <p>10$/hr</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Total Projects</label>
-                            </div>
-                            <div class="col-md-6">
-                                <p>230</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>English Level</label>
-                            </div>
-                            <div class="col-md-6">
-                                <p>Expert</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label>Availability</label>
-                            </div>
-                            <div class="col-md-6">
-                                <p>6 months</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <label>Your Bio</label><br/>
-                                <p>Your detail description</p>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </div>
 @stop
 @push('js')
-    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
-    <script>
-        $('#toggle').change(function() {
+<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
+<script>
+    $('#toggle').change(function() {
             var mode = $(this).prop('checked');
             var verify = (mode == true) ? 1 : 0;
             $.ajax({
@@ -176,5 +135,5 @@
                 }
             });
         });
-    </script>
+</script>
 @endpush
