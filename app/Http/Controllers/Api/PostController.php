b2cad232d6a5548e1054989fa0dd6f423256dbca
monachouhan <mona@amplework.com>
2025-03-12 09:58:07 +0530

    change notification message

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -1005,7 +1005,7 @@ class PostController extends Controller
         $deepLink = 'EXPOSVRE://postlike/' . $post->id;
 
         $notification = new \App\Models\Notification();
-        $notification->title = 'liked your post';
+        $notification->title = 'USER loved your post';
         $notification->description = 'like on your post';
         $notification->type = 'like';
         $notification->user_id = $post->owner_id;
