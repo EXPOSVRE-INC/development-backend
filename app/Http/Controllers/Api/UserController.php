f31365a179e9ebe632c2a64548ecdbe001352b53
monachouhan <mona@amplework.com>
2025-07-15 11:54:02 +0530

    work on filter own post on user profile post

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -936,6 +936,7 @@ class UserController extends Controller
         if ($type == 'market') {
             $marketPosts = Post::with(['owner'])
                 ->where('post_for_sale', 1)
+                ->where('owner_id', $user->id)
                 ->where(function ($query) {
                     $query->whereNull('status')
                         ->orWhere('status', '!=', 'archive');
